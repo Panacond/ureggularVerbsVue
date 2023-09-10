c3a5c6ce6b451de1a1868cd04a64c8f676635015
akakiy <ivachkevich63@gmail.com>
2023-09-10 23:45:52 +0300

    add 20 words

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 import RowHead from './components/RowHead.vue'
 import RowWord from './components/RowWord.vue'
 import wordsJson from './words.json'
+import Back from './components/Back.vue'
 </script>
 
 <template>
   <RowHead/>
   <RowWord v-for="i in wordsJson" :word="i"/>
+  <Back  :wordsJson="wordsJson"/>
 </template>
-
---
+++ src/components/Back.vue
@@ -0,0 +1,27 @@
+<script>
+// import wordsJson from '../words.json'
+export default {
+    props: {
+        wordsJson:Array
+    },
+    computed: {
+        lenList() {
+            return ((this.wordsJson.length + 1) * 45 + 100) + "px"
+            // return ( 440) + "px"
+        }
+    },
+}
+</script>
+<template>
+    <div class="back" :style="{ height: lenList }"></div>
+</template>
+<style scoped>
+.back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    /* background-color: red; */
+    width: 0.1px;
+    /* height: 4850px; */
+}
+</style>
